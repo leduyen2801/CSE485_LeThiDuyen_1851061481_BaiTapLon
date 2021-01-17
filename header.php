87ed175df1d5045b919078f1a9681ea56b787561
leduyen2801 <1851061481@e.tlu.edu.vn>
2021-01-17 18:56:42 +0700

    edit

---
+++ header.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>CV Cá Nhân</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body style="background:  #ee9dd35e;">
+    <div class='nav'>
+    <ul>
+        <li>
+        <a class='logo' href='#'>
+        </a>
+        </li>
+        <li>
+        <a href='index.php'>Trang chủ</a>
+        </li>
+        <li>
+        <a  href='about.php'>Giới thiệu</a>
+        </li>
+        <li>
+        <a href='resume.php'>Sơ yếu lý lịch</a>
+        </li>
+        <li>
+        <a href='contact.php'>Liên hệ</a>
+        </li>
+        <li>
+        <a href='blog.php'>Blog</a>
+        </li>
+        <li>
+        <a href='#contact'>
+            <div class='fa fa-envelope'></div>
+        </a>
+        </li>
+    </ul>
+    </div>
--- index.php
+++ index.php
@@ -1,61 +1,22 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>ssdfsd</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body style="background: #98eaf870;">
-    <div class='nav'>
-    <ul>
-        <li>
-        <a class='logo' href='http://andytran.me'>
-            <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/logo_(1).svg'>
-        </a>
-        </li>
-        <li>
-        <a href='index.php'>Home</a>
-        </li>
-        <li>
-        <a  href='about.php'>
-            About
-        </li>
-        <li>
-        <a href='#resume'>Resume</a>
-        </li>
-        <li>
-        <a href='#contact'>Contact</a>
-        </li>
-        <li>
-        <a href='#blog'>Blog</a>
-        </li>
-        <li>
-        <a href='#contact'>
-            <div class='fa fa-envelope'></div>
-        </a>
-        </li>
-    </ul>
-    </div>
-    <div class="container">
-        <div class="row" >
-            <div class="col-md-6">
-                <img src="images/VHD_7153.jpg">
-            </div>
-            <div class="col-md-6">
-                
-            </div>
+<?php
+    include("header.php");
+?>
+    <div class="row" style="background:  #ee9dd35e;">
+        <div class="col-md-6">
+        <br>
+            <img src="images/duyen.png">
+        </div>
+        <div class="col-md-6" >
+            <p>Xin chào!
+            <br>Mình là Lê Thị Duyên 
+            <br>Bạn đã biết gì về tôi? 
+            <br>Hãy cùng tìm hiểu nhé!!!</p> 
+            <a href='https://www.facebook.com/profile.php?id=100030899865523'><i class="fab fa-facebook-square"></i></a>
+            <a href='https://github.com/leduyen2801/CSE485_LeThiDuyen_1851061481_BaiTapLon.git'><i class="fab fa-github-square"></i></a>
         </div>
     </div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
+    
+<?php
+    include("footer.php");
+?>
